c7f38df86ab472abf63b23372afd04a41f9fcea6
azalea-evie-harper <azalea.leah.harper@gmail.com>
2024-07-14 20:17:55 +0100

    WIP make characteristics editable

--- src/VampireVault.Client/Components/Characteristic.razor.cs
+++ src/VampireVault.Client/Components/Characteristic.razor.cs
@@ -14,4 +14,27 @@ public partial class Characteristic
     
     [Parameter]
     public byte MaxValue { get; set; }
+
+    public int PointsToDistribute
+        => Value == 0
+            ? 3
+            : Value * 2;
+
+    public int PointsToRefund
+        => Value == 1
+            ? 3
+            : Math.Abs(Value - 1 * 2);
+    
+    public void RefundPoint()
+    {
+        Console.WriteLine("RefundPoint");
+        Value--;
+    }
+    
+    public void BuyPoint()
+    {
+        Console.WriteLine("BuyPoint");
+        
+        Value++;
+    }
 }
